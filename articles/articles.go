2f17f1c89abb6eb2446a455bd5b224615ccb4807
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-12-23 14:55:00 +0200

    feat: add read time for each article
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- articles/articles.go
+++ articles/articles.go
@@ -5,10 +5,11 @@ import (
 )
 
 type ArticleMetadata struct {
-	ID          uint64
-	Name        string
-	URL         string
-	Description string
+	ID              uint64
+	Name            string
+	URL             string
+	Description     string
+	ReadTimeMinutes int
 }
 
 var (
@@ -24,18 +25,21 @@ var Metadata = []ArticleMetadata{
 		URL:         "/p/public/articles/" + strconv.FormatUint(AnsiblePlusTailsclaleEqualGreatComboID, 10),
 		Name:        "Ansible + Tailscale = 🎉 ",
 		Description: "Manage VMs in a private network with Ansible and Tailscale.",
+		ReadTimeMinutes: 4,
 	},
 	{
 		ID:          ZeroTrustHomelabV2ID,
 		URL:         "/p/public/articles/" + strconv.FormatUint(ZeroTrustHomelabV2ID, 10),
 		Name:        "Zero trust homelab V2",
 		Description: "An updated version of my homelab setup using FluxCD, Doppler and my own Terraform provider.",
+		ReadTimeMinutes: 6,
 	},
 	{
 		ID:          ZeroTrustHomelabID,
 		URL:         "/p/public/articles/" + strconv.FormatUint(ZeroTrustHomelabID, 10),
 		Name:        "Zero trust homelab",
 		Description: "My homelab setup using Terraform, Helm, Cloudflare, Tailscale and more...",
+		ReadTimeMinutes: 9,
 	},
 }
 
